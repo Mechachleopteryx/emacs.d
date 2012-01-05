45d6c6b0b4758f268eab3426f11cd19c9fb67737
Tavis Rudd <tavis@damnsimple.com>
2012-01-04 16:00:24 -0800

    require magit and a few new funcs for clear invalid vc state

--- dss-vc.el
+++ dss-vc.el
@@ -2,6 +2,7 @@
 (require 'ediff)
 (require 'ediff-vers)
 (require 'vc)
+(require 'magit)
 
 (setq dvc-tips-enabled nil)
 (setq vc-follow-symlinks t)
@@ -12,5 +13,20 @@
   (ediff-load-version-control)
   (setq ediff-split-window-function 'split-window-horizontally)
   (ediff-vc-internal "" ""))
+
+(defun dss/vc-state-refresh-open-buffers ()
+  (interactive)
+  (message "updating buffer VC status ...")
+  (mapc (lambda (b)
+          (dss/vc-state-refresh (buffer-file-name b)))
+        (buffer-list))
+  (message ""))
+
+(defun dss/vc-state-refresh (file &optional backend)
+  (interactive)
+  (when (> (length file) 0)
+    (setq backend (or backend (vc-backend file)))
+    (when backend
+      (vc-state-refresh file backend))))
 
 (provide 'dss-vc)
